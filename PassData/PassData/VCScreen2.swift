344081ac5d5b79b322a679542334c5ad6257779e
Trần Đạt <trandat180396@gmail.com>
2018-12-25 17:25:24 +0700

    UpdateCodePushNotification

--- PassData/PassData/VCScreen2.swift
+++ PassData/PassData/VCScreen2.swift
@@ -1,12 +1,10 @@
 import UIKit
 class VCScreen2: UIViewController {
-    
-    var show : ((_ text: String) -> ())?
 
     @IBOutlet weak var tfShow: UITextField!
     
     @IBAction func enter(_ sender: UIButton) {
-        show?(tfShow.text ?? "")
+        NotificationCenter.default.post(name: .lblShow, object: nil, userInfo: ["name": tfShow.text ?? ""])
     navigationController?.popViewController(animated: true)
     }
 }
--- PassData/PassData/ViewController.swift
+++ PassData/PassData/ViewController.swift
@@ -4,12 +4,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var lblShow: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(lblShow(notification:)), name: .lblShow, object: nil)
+    }
+    
+    @objc func lblShow(notification: Notification){
+        if let text = notification.userInfo {
+            if let choose = text["name"] as? String {
+                lblShow.text = choose
+            }
+        }
+    }
+    
     @IBAction func OK(_ sender: UIButton) {
         let db = UIStoryboard(name: "Main", bundle: nil)
         let screen = db.instantiateViewController(withIdentifier: "screen2") as! VCScreen2
-        screen.show = { [weak self] (text) in
-            self?.lblShow.text = text
-        }
     navigationController?.pushViewController(screen, animated: true)
     }
+}
+
+extension Notification.Name {
+    static let lblShow = Notification.Name("lblShow")
 }
